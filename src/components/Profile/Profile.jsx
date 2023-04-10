9c7c1077fb4dbd0c79648b24935f919d0bbc517d
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-04-10 10:49:41 +0300

    Update Profile.jsx

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -49,7 +49,7 @@ Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
   location: PropTypes.string.isRequired,
-  avatar: PropTypes.string,
+  avatar: PropTypes.string.isRequired,
   stats: PropTypes.exact({
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
