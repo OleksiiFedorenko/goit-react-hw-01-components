b923a91e0ba8f75fdd75af4ebef63d3b24ea001e
Oleksii Fedorenko <fedorenkolesha@gmail.com>
2023-04-10 10:45:22 +0300

    Update profile props types

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -46,15 +46,13 @@ export const Profile = ({
 };
 
 Profile.propTypes = {
-  user: PropTypes.exact({
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string,
-    stats: PropTypes.exact({
-      followers: PropTypes.number.isRequired,
-      views: PropTypes.number.isRequired,
-      likes: PropTypes.number.isRequired,
-    }),
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string,
+  stats: PropTypes.exact({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
   }),
 };
